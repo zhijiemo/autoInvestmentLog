58f46c1f0900f822dc737a5d07614e8130d5e28e
zhijiemo <750282510@qq.com>
2021-09-16 21:04:03 +0800

    Update extractData.py

--- extractData.py
+++ extractData.py
@@ -12,7 +12,7 @@ report = str()
 
 stocks_codes_str = config.get('stocks', 'codes')
 stocks_codes_list = stocks_codes_str.replace(' ', '').split(',')
-df_stocks = pd.read_excel(localtime + '/A_data.xlsx', dtype='object')[['code', 'name', 'trade', 'changepercent']]
+df_stocks = pd.read_excel(localtime + '/A_data.xls', dtype='object')[['code', 'name', 'trade', 'changepercent']]
 for stock_code in stocks_codes_list:
     if stock_code == '':
         continue
@@ -29,7 +29,7 @@ for stock_code in stocks_codes_list:
 
 inner_goods_future_str = config.get('InnerGoodsFutures', 'names')
 inner_goods_future_list = inner_goods_future_str.replace(' ', '').split(',')
-df_inner_goods = pd.read_excel(localtime + '/Future_C_data.xlsx', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
+df_inner_goods = pd.read_excel(localtime + '/Future_C_data.xls', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
 df_inner_goods['change'] = 100 * (df_inner_goods['current_price'].apply(float) - df_inner_goods['last_settle_price'].apply(float)) / df_inner_goods['last_settle_price'].apply(float)
 for name in inner_goods_future_list:
     if name == '':
@@ -45,7 +45,7 @@ for name in inner_goods_future_list:
 
 inner_finance_future_str = config.get('InnerFinanceFutures', 'names')
 inner_finance_future_list = inner_finance_future_str.replace(' ', '').split(',')
-df_inner_finance = pd.read_excel(localtime + '/Future_F_data.xlsx', dtype='object')[['symbol', 'current_price', 'open']]
+df_inner_finance = pd.read_excel(localtime + '/Future_F_data.xls', dtype='object')[['symbol', 'current_price', 'open']]
 df_inner_finance['change'] = 100 * (df_inner_finance['current_price'].apply(float) - df_inner_finance['open'].apply(float)) / df_inner_finance['open'].apply(float)
 for name in inner_finance_future_list:
     if name == '':
@@ -61,7 +61,7 @@ for name in inner_finance_future_list:
 
 outer_future_str = config.get('OuterFutures', 'names')
 outer_future_list = outer_future_str.replace(' ', '').split(',')
-df_outer = pd.read_excel(localtime + '/Future_W_data.xlsx', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
+df_outer = pd.read_excel(localtime + '/Future_W_data.xls', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
 df_outer['change'] = 100 * (df_outer['current_price'].apply(float) - df_outer['last_settle_price'].apply(float)) / df_outer['last_settle_price'].apply(float)
 for name in outer_future_list:
     if name == '':
@@ -79,7 +79,7 @@ shibor_time_str = config.get('Shibor', 'times')
 shibor_time_list = shibor_time_str.replace(' ', '').split(',')
 for t in shibor_time_list:
     try:
-        df_temp = pd.read_excel(localtime + '/Shibor_' + t + '.xlsx', index_col=0)
+        df_temp = pd.read_excel(localtime + '/Shibor_' + t + '.xls', index_col=0)
         rate = df_temp['利率(%)'][0]
         bp = df_temp['涨跌(BP)'][0]
         report += "Shibor{ti}  利率 {rate}%, 涨跌{bp}BP\r\n\r\n"\
@@ -91,7 +91,7 @@ libor_time_str = config.get('Libor', 'times')
 libor_time_list = libor_time_str.replace(' ', '').split(',')
 for t in libor_time_list:
     try:
-        df_temp = pd.read_excel(localtime + '/Libor_' + t + '.xlsx', index_col=0)
+        df_temp = pd.read_excel(localtime + '/Libor_' + t + '.xls', index_col=0)
         rate = df_temp['利率(%)'][0]
         bp = df_temp['涨跌(BP)'][0]
         report += "Libor美元 {ti}  利率 {rate}%, 涨跌{bp}BP\r\n\r\n"\
