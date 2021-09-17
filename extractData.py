163557c12d85e13bfaa804e99169f4d2080af594
zhijiemo <750282510@qq.com>
2021-09-17 23:05:13 +0800

    Update extractData.py

--- extractData.py
+++ extractData.py
@@ -5,100 +5,39 @@ import datetime
 import pytz
 
 localtime = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d")
+Outfilename="Data-"+str(localtime)+".txt"
 config = configparser.ConfigParser()
 filename = 'config.ini'
 config.read(filename, encoding='utf-8')
 report = str()
 
+report +="个股：\r\n"
 stocks_codes_str = config.get('stocks', 'codes')
 stocks_codes_list = stocks_codes_str.replace(' ', '').split(',')
-df_stocks = pd.read_excel(localtime + '/A_data.xls', dtype='object')[['code', 'name', 'trade', 'changepercent']]
+# df_stocks = pd.read_excel(localtime + '/A_data.xlsx', dtype='object')[['code', 'name', 'trade', 'changepercent']]
+df_stocks = pd.read_excel(localtime + '/A_data.xlsx', dtype='object')[['代码',	'名称',	'最新价',	'涨跌额',	'涨跌幅',	'买入',	'卖出',	'昨收',	'今开',	'最高',	'最低',	'成交量',	'成交额']]
 for stock_code in stocks_codes_list:
     if stock_code == '':
         continue
     try:
-        df_temp = df_stocks[df_stocks['code'] == stock_code].reset_index(drop=True)
-        name = df_temp['name'][0]
-        trade = df_temp['trade'][0]
-        change = df_temp['changepercent'][0]
-        report += "{code} {name}  现价 {close:.2f}, {vhdp}{change:.2f}%\r\n\r\n"\
-            .format(code=stock_code, name=name, close=float(trade)
-                    , vhdp='上涨' if float(change) >= 0 else '下跌', change=abs(float(change)))
+        # df_temp = df_stocks[df_stocks['code'] == stock_code].reset_index(drop=True)
+        # name = df_temp['name'][0]
+        # trade = df_temp['trade'][0]
+        # change = df_temp['changepercent'][0]
+        df_temp = df_stocks[df_stocks['代码'] .str.contains(stock_code)].reset_index(drop=True)
+        name = df_temp['名称'][0]
+        current_price = df_temp['最新价'][0]
+        price_change = df_temp['涨跌额'][0]
+        price_change_percent = df_temp['涨跌幅'][0]
+        last_price = df_temp['昨收'][0]
+        max_price = df_temp['最高'][0]
+        min_price = df_temp['最低'][0]
+        trade_volume = df_temp['成交量'][0]
+        trade_amount = df_temp['成交额'][0]
+        report += "{代码} {名称}  最新价 {current_price:.2f}, {vhdp}{price_change_percent:.2f}%\r\n"\
+            .format(代码=stock_code, 名称=name, current_price=float(current_price)
+                    , vhdp='上涨' if float(price_change_percent) >= 0 else '下跌', price_change_percent=abs(float(price_change_percent)))
     except:
         continue
-
-inner_goods_future_str = config.get('InnerGoodsFutures', 'names')
-inner_goods_future_list = inner_goods_future_str.replace(' ', '').split(',')
-df_inner_goods = pd.read_excel(localtime + '/Future_C_data.xls', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
-df_inner_goods['change'] = 100 * (df_inner_goods['current_price'].apply(float) - df_inner_goods['last_settle_price'].apply(float)) / df_inner_goods['last_settle_price'].apply(float)
-for name in inner_goods_future_list:
-    if name == '':
-        continue
-    try:
-        df_temp = df_inner_goods[df_inner_goods['symbol'].str.contains(name)].reset_index(drop=True)
-        price = df_temp['current_price'][0]
-        change = df_temp['change'][0]
-        report += "{name}  现价 {price:.2f}, {vhdp}{change:.2f}%\r\n\r\n"\
-            .format(name=name, price=float(price), change=abs(float(change)), vhdp='上涨' if float(change) >=0 else '下跌')
-    except:
-        continue
-
-inner_finance_future_str = config.get('InnerFinanceFutures', 'names')
-inner_finance_future_list = inner_finance_future_str.replace(' ', '').split(',')
-df_inner_finance = pd.read_excel(localtime + '/Future_F_data.xls', dtype='object')[['symbol', 'current_price', 'open']]
-df_inner_finance['change'] = 100 * (df_inner_finance['current_price'].apply(float) - df_inner_finance['open'].apply(float)) / df_inner_finance['open'].apply(float)
-for name in inner_finance_future_list:
-    if name == '':
-        continue
-    try:
-        df_temp = df_inner_finance[df_inner_finance['symbol'].str.contains(name)].reset_index(drop=True)
-        price = df_temp['current_price'][0]
-        change = df_temp['change'][0]
-        report += "{name}  现价 {price:.2f}, {vhdp}{change:.2f}%\r\n\r\n"\
-            .format(name=name, price=float(price), change=abs(float(change)), vhdp='上涨' if float(change) >=0 else '下跌')
-    except:
-        continue
-
-outer_future_str = config.get('OuterFutures', 'names')
-outer_future_list = outer_future_str.replace(' ', '').split(',')
-df_outer = pd.read_excel(localtime + '/Future_W_data.xls', dtype='object')[['symbol', 'current_price', 'last_settle_price']]
-df_outer['change'] = 100 * (df_outer['current_price'].apply(float) - df_outer['last_settle_price'].apply(float)) / df_outer['last_settle_price'].apply(float)
-for name in outer_future_list:
-    if name == '':
-        continue
-    try:
-        df_temp = df_outer[df_outer['symbol'].str.contains(name)].reset_index(drop=True)
-        price = df_temp['current_price'][0]
-        change = df_temp['change'][0]
-        report += "{name}  现价 {price:.2f}, {vhdp}{change:.2f}%\r\n\r\n"\
-            .format(name=name, price=float(price), change=abs(float(change)), vhdp='上涨' if float(change) >=0 else '下跌')
-    except:
-        continue
-
-shibor_time_str = config.get('Shibor', 'times')
-shibor_time_list = shibor_time_str.replace(' ', '').split(',')
-for t in shibor_time_list:
-    try:
-        df_temp = pd.read_excel(localtime + '/Shibor_' + t + '.xls', index_col=0)
-        rate = df_temp['利率(%)'][0]
-        bp = df_temp['涨跌(BP)'][0]
-        report += "Shibor{ti}  利率 {rate}%, 涨跌{bp}BP\r\n\r\n"\
-            .format(ti=t, rate=rate, bp=bp)
-    except:
-        continue
-
-libor_time_str = config.get('Libor', 'times')
-libor_time_list = libor_time_str.replace(' ', '').split(',')
-for t in libor_time_list:
-    try:
-        df_temp = pd.read_excel(localtime + '/Libor_' + t + '.xls', index_col=0)
-        rate = df_temp['利率(%)'][0]
-        bp = df_temp['涨跌(BP)'][0]
-        report += "Libor美元 {ti}  利率 {rate}%, 涨跌{bp}BP\r\n\r\n"\
-            .format(ti=t, rate=rate, bp=bp)
-    except:
-        continue
-
-
-with open('todayData.txt', 'w', encoding='utf-8') as f:
+with open(Outfilename, 'w', encoding='utf-8') as f:
     f.write(report)
