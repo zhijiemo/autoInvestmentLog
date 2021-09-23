650a3ea9fde8ff5fd99232cd1d46afb314bf53c4
zhijiemo <750282510@qq.com>
2021-09-23 17:02:38 +0800

    Update GetAndExtract.py

--- GetAndExtract.py
+++ GetAndExtract.py
@@ -24,26 +24,26 @@ try:
     for stock_code in stocks_codes_list:
         if stock_code == '':
             continue
-    try:
-        df_temp = df_stocks[df_stocks['代码'] .str.contains(stock_code)].reset_index(drop=True)
-        name = df_temp['名称'][0]
-        current_price = df_temp['最新价'][0]
-        price_change = df_temp['涨跌额'][0]
-        price_change_percent = df_temp['涨跌幅'][0]
-        last_price = df_temp['昨收'][0]
-        max_price = df_temp['最高'][0]
-        min_price = df_temp['最低'][0]
-        trade_volume = df_temp['成交量'][0]
-        trade_amount = df_temp['成交额'][0]
-        report += "{代码} {名称}  最新价 {current_price:.2f}, {vhdp}{price_change_percent:.2f}%\r\n"\
-            .format(代码=stock_code, 名称=name, current_price=float(current_price)
-                    , vhdp='上涨' if float(price_change_percent) >= 0 else '下跌', price_change_percent=abs(float(price_change_percent)))
-    except:
-        continue
+         try:
+            df_temp = df_stocks[df_stocks['代码'] .str.contains(stock_code)].reset_index(drop=True)
+            name = df_temp['名称'][0]
+            current_price = df_temp['最新价'][0]
+            price_change = df_temp['涨跌额'][0]
+            price_change_percent = df_temp['涨跌幅'][0]
+            last_price = df_temp['昨收'][0]
+            max_price = df_temp['最高'][0]
+            min_price = df_temp['最低'][0]
+            trade_volume = df_temp['成交量'][0]
+            trade_amount = df_temp['成交额'][0]
+            report += "{代码} {名称}  最新价 {current_price:.2f}, {vhdp}{price_change_percent:.2f}%\r\n"\
+                .format(代码=stock_code, 名称=name, current_price=float(current_price)
+                        , vhdp='上涨' if float(price_change_percent) >= 0 else '下跌', price_change_percent=abs(float(price_change_percent)))
+        except:
+            continue
+    with open('todayData.txt', 'w', encoding='utf-8') as f:
+        f.write(report)
 except:
     pass
 # with open(Outfilename, 'w', encoding='utf-8') as f:
-with open('todayData.txt', 'w', encoding='utf-8') as f:
-    f.write(report)
 
 time.sleep(10)
